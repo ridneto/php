e4da21b9a10837692dc53f2f0c7eaa63dad74f4c
Ridneto <ridineu@hotmail.com>
2017-10-11 14:43:03 -0300

    operadores

---
+++ operadores/exemplo03.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 30;
+$b = 20;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a == $b);
+
+echo "<br>";
+
+var_dump($a === $b);
+
+echo "<br>";
+
+var_dump($a !== $b);
+
+?>
