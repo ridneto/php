149572d23643b5735cea8b03b04a756b30b3c79c
Ridneto <ridineu@hotmail.com>
2017-10-11 15:04:22 -0300

    include

---
+++ include/exemplo02.php
@@ -0,0 +1,12 @@
+<?php
+
+//include "exemplo01.php";
+
+require_once "inc/exemplo01.php";
+require_once "inc/exemplo01.php";
+
+$resultado = somar(10, 20);
+
+echo $resultado;
+
+?>
---
+++ include/inc/exemplo01.php
@@ -0,0 +1,9 @@
+<?php
+
+var_dump("ok");
+
+function somar($x, $y){
+	return $x + $y;
+}
+
+?>
