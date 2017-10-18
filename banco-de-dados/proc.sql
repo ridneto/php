4682dda2fc719fbb1b432e0806598f833090b81b
Ridneto <ridineu@hotmail.com>
2017-10-18 16:00:08 -0200

    procedure

---
+++ banco-de-dados/proc.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE `sp_usuarios_insert` (
+pdeslogin VARCHAR(64),
+pdessenha VARCHAR(256)
+)
+BEGIN
+	INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (pdeslogin, pdessenha);
+    
+    SELECT * FROM tb_usuarios WHERE idusuario = LAST_INSERT_ID();
+END
